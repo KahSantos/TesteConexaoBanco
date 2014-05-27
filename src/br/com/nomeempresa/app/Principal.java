2f11ef05f9bf190e9192d9952281186df4302b84
karen_luiz <karen_luiz@10.1.48.250>
2014-05-27 16:35:04 -0300

    Teste de conexao banco

---
+++ src/br/com/nomeempresa/app/Principal.java
@@ -0,0 +1,23 @@
+
+package br.com.nomeempresa.app;
+
+import br.com.nomeempresa.modelo.dao.FabricadeConexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Principal {
+    
+    public static void main(String[] args) throws SQLException {
+        
+       Connection conn = null;
+        conn = FabricadeConexao.pegaConexao();
+        PreparedStatement pstm = null;
+        pstm = FabricadeConexao.pegaConexao()
+                .prepareStatement("insert into "
+                + "Funcionario (nome, telefone)"
+                + "values ('João', '48 99896924');");
+        
+        pstm.execute();
+    }
+}
---
+++ src/br/com/nomeempresa/modelo/Funcionario.java
@@ -0,0 +1,7 @@
+
+package br.com.nomeempresa.modelo;
+
+public class Funcionario extends Pessoa {
+    
+    
+}
